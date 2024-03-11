06fabed0a921056dfdf84014703454b3eda55cdf
Mohammad Bozorgmehr <mohammad.bzr82@gmail.com>
2024-03-11 15:01:16 +0330

    create primary button

--- src/Components/Ui/PrimaryButton.tsx
+++ src/Components/Ui/PrimaryButton.tsx
@@ -8,9 +8,9 @@ interface ComponentProps {
 const PrimaryButton: React.FC<ComponentProps> = ({ children, className }) => {
 	return (
 		<button
-			className={` bg-gradient-to-r from-[#8176AF] to-[#C0B7E8] ${className} w-[155px] h-[48px] text-black rounded-3xl flex justify-center items-center`}
+			className={` bg-gradient-to-r from-[#8176AF] to-[#C0B7E8] ${className} w-[155px] h-[48px] text-black rounded-3xl flex justify-center items-center hover:shadow-xl  `}
 		>
-			{children}
+			<p className="font-Oswald  font-bold text-[#302C42]">{children}</p>
 		</button>
 	);
 };
